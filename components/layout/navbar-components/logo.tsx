fe32d7b8509ec3797aa248fe2525b4551a6ae283
rodobkn <rrmendoza@uc.cl>
2025-05-26 00:38:48 -0400

    improving navbar and configure shadcn

---
+++ components/layout/navbar-components/logo.tsx
@@ -0,0 +1,10 @@
+export const Logo = () => {
+  return (
+    <a
+      href="/"
+      className="font-bold text-xl cursor-pointer"
+    >
+      My E-commerce
+    </a>
+  )
+}
--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -1,11 +1,13 @@
+import { Logo } from "@/components/layout/navbar-components/logo";
+import { UserSettings } from "@/components/auth/user-settings";
 
 export const Navbar = () => {
-    return (
-        <nav className="w-full bg-[#0f1111] text-white">
-            <div className="flex items-center justify-between px-4 py-3">
-                <div>Logo</div>
-                <div>Profile</div> 
-            </div>
-        </nav>
-    )
+  return (
+    <nav className="w-full bg-[#0f1111] text-white">
+      <div className="flex items-center justify-between px-4 py-3">
+        <Logo />
+        <UserSettings />
+      </div>
+    </nav>
+  )
 }
