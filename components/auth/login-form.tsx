cc3548e2e011461430d525c975ea0480360919fa
rodobkn <rrmendoza@uc.cl>
2025-05-27 20:17:52 -0400

    adding the auth frontend 2

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -1,8 +1,37 @@
 "use client";
 
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { useState  } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Input } from "@/components/ui/input";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage
+} from "@/components/ui/form";
+import { Button } from "@/components/ui/button";
 import { AuthCardWrapper } from "@/components/auth/auth-card-wrapper";
+import { FormError } from "@/components/errors/form-error";
+import { LoginType } from "@/validation-types/auth-types";
 
 export const LoginForm = () => {
+  const [error, setError] = useState("");
+
+  const form = useForm<z.infer<typeof LoginType>>({
+    resolver: zodResolver(LoginType),
+    defaultValues: {
+      email: "",
+      password: "",
+    }
+  })
+
+  const onSubmit = (values: z.infer<typeof LoginType>) => {
+    console.log("Usuario iniciando sesion con los siguientes values: ", values);
+  }
 
   return (
     <AuthCardWrapper
@@ -12,7 +41,56 @@ export const LoginForm = () => {
       switchButtonHref="/auth/register"
       showSocial={true}
     >
-      <div>Children Login Placeholder</div>
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="space-y-6"
+        >
+          <div className="space-y-4">
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      placeholder="juanito@gmail.com"
+                      type="email"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Contraseña</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      placeholder="******"
+                      type="password"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <FormError message={error} />
+          <Button
+            type="submit"
+            className="w-full"
+          >
+            Iniciar Sesión
+          </Button>
+        </form>
+      </Form>
     </AuthCardWrapper>
   )
 
--- components/auth/register-form.tsx
+++ components/auth/register-form.tsx
@@ -1,8 +1,39 @@
 "use client";
 
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { useState  } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Input } from "@/components/ui/input";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage
+} from "@/components/ui/form";
+import { Button } from "@/components/ui/button";
 import { AuthCardWrapper } from "@/components/auth/auth-card-wrapper";
+import { FormError } from "@/components/errors/form-error";
+import { RegisterType } from "@/validation-types/auth-types";
 
 export const RegisterForm = () => {
+  const [error, setError] = useState("");
+
+  const form = useForm<z.infer<typeof RegisterType>>({
+    resolver: zodResolver(RegisterType),
+    defaultValues: {
+      email: "",
+      password: "",
+      name: "",
+    }
+  })
+
+  const onSubmit = (values: z.infer<typeof RegisterType>) => {
+    console.log("Usuario registrandose con los siguientes values: ", values);
+  }
+
 
   return (
     <AuthCardWrapper
@@ -12,7 +43,72 @@ export const RegisterForm = () => {
       switchButtonHref="/auth/login"
       showSocial={true}
     >
-      <div>Children Register Placeholder</div>
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="space-y-6"
+        >
+          <div className="space-y-4">
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Nombre</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      placeholder="juanito"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      placeholder="juanito@gmail.com"
+                      type="email"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Contraseña</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      placeholder="******"
+                      type="password"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <FormError message={error} />
+          <Button
+            type="submit"
+            className="w-full"
+          >
+            Registrar Cuenta
+          </Button>
+        </form>
+      </Form>
     </AuthCardWrapper>
   )
 
